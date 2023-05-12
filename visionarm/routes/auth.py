a98280f1b80b81ecb6b92f04d80f610e29405d4d
Ihor Bytskalo <igor.bytskalo@gmail.com>
2023-05-12 13:45:54 +0300

    Added registration, login, q&a system

---
+++ visionarm/routes/auth.py
@@ -0,0 +1,48 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from werkzeug.security import check_password_hash
+from flask_login import login_user, logout_user
+
+from visionarm.extentions import db
+from visionarm.models import User
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        login = request.form['login']
+        unhashed_password = request.form['password']
+        name = request.form['name']
+        surname = request.form['surname']
+
+        user = User(login=login, unhashed_password=unhashed_password, name=name, surname=surname, admin=False, expert=False)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+    return render_template('register.html')
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        login = request.form['login']
+        password = request.form['password']
+        name = request.form['name']
+        surname = request.form['surname']
+
+        user = User.query.filter_by(login=login).first()
+
+        error_message = ''
+
+        if not user or not check_password_hash(user.password, password):
+            error_message = 'Could not log in. Please check the information and try again.'
+
+        if not error_message:
+            login_user(user)
+            return redirect(url_for('main.index'))
+        
+    return render_template('login.html')
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
